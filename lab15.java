7715d1c9c5d543f990bdccc811e14c255948e951
ussreddy <69449156+ussreddy@users.noreply.github.com>
2020-11-29 09:29:05 +0530

    Add files via upload

---
+++ lab15.java
@@ -0,0 +1,35 @@
+/*write a program to  give a simple example for abstract class*/
+ abstract class shape
+ {
+   abstract void area();
+    }
+     class rectangle extends shape
+      {
+         public void area ()
+         {
+           int l=2,b=3;
+           int a=l*b;
+           system.out.println("area of rectangle"+a);
+            }
+          }
+           class square extends shape
+           {
+              public void area
+               {
+                 int side=2;
+                 int a=side*side;
+                  system.out.println("area of square"+a);
+                   }
+                  }
+                    class lab15
+                      {
+                          public static void main(string args[])
+                                {
+                                     rectangle static void main(string args[])
+                               }
+                        rectange r=new rectangle();
+                            r.area();
+                        square s=new square();
+                            s.area();  
+                 }
+} 
