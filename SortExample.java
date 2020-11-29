98431b1f03f775282e78a5b726b51bde217d01e9
ussreddy <69449156+ussreddy@users.noreply.github.com>
2020-11-29 09:07:16 +0530

    Create SortExample.java

---
+++ SortExample.java
@@ -0,0 +1,15 @@
+import java.util.Arrays; 
+
+public class SortExample 
+{ 
+    public static void main(String[] args) 
+    { 
+
+        int[] arr = {111,12,28,4,0,77,3,6 }; 
+
+        Arrays.sort(arr); 
+
+        System.out.printf("sorted arr[] : %s", 
+                          Arrays.toString(arr)); 
+    } 
+} 
