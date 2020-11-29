b10dcd39ace8be376e055d5499640040ae5811a1
ussreddy <69449156+ussreddy@users.noreply.github.com>
2020-11-29 09:11:21 +0530

    Create Duplicate.java

---
+++ Duplicate.java
@@ -0,0 +1,25 @@
+public class Duplicate
+{
+    static int removeDuplicates(int arr[], int n) 
+    { 
+    	if(n==0||n==1) 
+        	return n; 
+        int[] temp = new int[n]; 
+        int j=0; 
+        for(int i=0;i<n-1;i++) 
+            if(arr[i]!=arr[i+1]) 
+                temp[j++]=arr[i]; 
+                temp[j++]=arr[n-1];
+		for(int i=0;i<j;i++) 
+            arr[i]=temp[i]; 
+        return j; 
+    } 
+public static void main(String[] args)  
+{ 
+        int arr[]={2, 6, 2, 8, 4, 12, 4, 12, 5}; 
+        int n=arr.length; 
+        n=removeDuplicates(arr,n); 
+ 	for (int i=0;i<n;i++) 
+           System.out.print(arr[i]+" "); 
+} 
+} 
